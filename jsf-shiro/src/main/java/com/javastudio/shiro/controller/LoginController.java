ff772bda5180daad5f936a84b506f30c7e08030a
Hossein Mohammadi <hossein.mohammadi@outlook.com>
2021-02-27 00:34:14 +0330

    javaee security

---
+++ jsf-shiro/src/main/java/com/javastudio/shiro/controller/LoginController.java
@@ -0,0 +1,11 @@
+package com.javastudio.shiro.controller;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class LoginController {
+    private String username;
+
+}
---
+++ jsf-shiro/src/main/java/com/javastudio/shiro/security/ShiroEnvironmentLoaderListener.java
@@ -0,0 +1,33 @@
+package com.javastudio.shiro.security;
+
+import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
+import org.apache.shiro.mgt.RealmSecurityManager;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.web.env.EnvironmentLoaderListener;
+import org.apache.shiro.web.env.WebEnvironment;
+import org.slf4j.Logger;
+
+import javax.inject.Inject;
+import javax.servlet.ServletContext;
+
+public class ShiroEnvironmentLoaderListener extends EnvironmentLoaderListener {
+    @Inject
+    private Logger logger;
+
+    @Inject
+    DatabaseRealm realm;
+
+    @Override
+    protected WebEnvironment createEnvironment(ServletContext sc) {
+        WebEnvironment env = super.createEnvironment(sc);
+        SecurityManager securityManager = env.getSecurityManager();
+
+        if (securityManager instanceof RealmSecurityManager) {
+            logger.info("Setting realm with hash algorithm SHA-256");
+            realm.setCredentialsMatcher(new HashedCredentialsMatcher("SHA-256"));
+            ((RealmSecurityManager) securityManager).setRealm(realm);
+        }
+
+        return env;
+    }
+}
