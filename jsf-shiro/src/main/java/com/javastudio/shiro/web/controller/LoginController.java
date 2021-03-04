15707d205ac513836db29414f2d29a7c68f5647b
Hossein Mohammadi <hossein.mohammadi@outlook.com>
2021-03-05 00:55:00 +0330

    javaee security - shiro authentication

--- jsf-shiro/src/main/java/com/javastudio/shiro/web/controller/LoginController.java
+++ jsf-shiro/src/main/java/com/javastudio/shiro/web/controller/LoginController.java
@@ -4,6 +4,8 @@ import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
+import org.apache.shiro.web.util.SavedRequest;
+import org.apache.shiro.web.util.WebUtils;
 import org.slf4j.Logger;
 
 import javax.faces.application.FacesMessage;
@@ -35,12 +37,15 @@ public class LoginController implements Serializable {
 
         try {
             currentUser.login(token);
-            return "/index?faces-redirect=true";
         } catch (AuthenticationException e) {
             FacesContext.getCurrentInstance().addMessage(null,
                     new FacesMessage(FacesMessage.SEVERITY_ERROR, "Invalid credential", ""));
         }
-        return null;
+
+        SavedRequest savedRequest = WebUtils.getAndClearSavedRequest(null);
+        logger.info("redirecting to " + savedRequest.getRequestURI() + "?faces-redirect=true");
+
+        return savedRequest.getRequestURI() + "?faces-redirect=true";
     }
 
     public String getUsername() {
